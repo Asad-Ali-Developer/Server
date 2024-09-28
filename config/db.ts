6fb0a136859e48746f95a38cc441be2472d4d6cd
Asad <uhope1645@gmail.com>
2024-09-28 12:17:00 +0500

    Did some changes

--- config/db.ts
+++ config/db.ts
@@ -4,17 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-const MONGODB_URI = 'mongodb+srv://tenant:tenant512@tenants-data-record.l4tm3.mongodb.net/?retryWrites=true&w=majority&appName=tenants-data-record'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://tenant:tenant512@tenants-data-record.l4tm3.mongodb.net/?retryWrites=true&w=majority&appName=tenants-data-record'
 
 
+// Connect to MongoDB
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGODB_URI)
+        await mongoose.connect(MONGODB_URI);
         console.log('Connected to MongoDB!');
     } catch (error) {
-        console.log(error);
-        process.exit(0) // It will exit the process when having error
+        console.error('MongoDB connection error:', error);
+        process.exit(1); // Exit the process on error
     }
-}
+};
+
 
 export default connectDB;
